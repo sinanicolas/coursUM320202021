2ecac232934d8ee47143a8d0b63b7a58196a0cce
adriengicquel <gicquel.adrien29@gmail.com>
2020-10-23 10:46:04 +0200

    test jsonParse

--- scriptprojet.js
+++ scriptprojet.js
@@ -1,1 +1,8 @@
-console.log("c'est ici qu'il faut coder votre javascript")
+fetch(JSON.parse('https://raw.githubusercontent.com/jpatokal/openflights/master/data/airlines.dat'), { method: 'GET',
+               headers: {},
+               mode: 'cors',
+               cache: 'default'}).then(
+    function(response){
+        response.text().then(function(data){result = data})
+            }
+)
