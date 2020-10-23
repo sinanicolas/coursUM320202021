8bdccf4397b664162c1116422576dfa528139b4e
adriengicquel <gicquel.adrien29@gmail.com>
2020-10-23 15:48:32 +0200

    début affichage données de fichier json

--- scriptprojet.js
+++ scriptprojet.js
@@ -1,4 +1,5 @@
-fetch(JSON.parse('https://raw.githubusercontent.com/jpatokal/openflights/master/data/airlines.dat'), { method: 'GET',
+fetch('https://trouver.datasud.fr/dataset/973765d0-a2a8-442d-bf3b-aec4e70fdd69/resource/faa81490-2a38-42e5-8a43-3b326ba5fce9/download/geoserver-getfeature.application', 
+    { method: 'GET',
                headers: {},
                mode: 'cors',
                cache: 'default'}).then(
@@ -6,3 +7,5 @@ fetch(JSON.parse('https://raw.githubusercontent.com/jpatokal/openflights/master/
         response.text().then(function(data){result = data})
             }
 )
+
+
