bef1dd65ab5b6fec155e037ee4b702062b6bf26f
Berk Kayin <berkkayin@MacBook-Pro-Berk.home>
2020-10-23 14:45:32 +0200

    premier test api

--- scriptprojet.js
+++ scriptprojet.js
@@ -1,1 +1,14 @@
 console.log("c'est ici qu'il faut coder votre javascript")
+
+var result;
+fetch('https://trouver.datasud.fr/dataset/973765d0-a2a8-442d-bf3b-aec4e70fdd69/resource/faa81490-2a38-42e5-8a43-3b326ba5fce9/download/geoserver-getfeature.application', { method: 'GET',
+               headers: {},
+               mode: 'cors',
+               cache: 'default'}).then(
+    function(response){
+        response.json().then(function(data){
+          console.log(data);
+          result = data;
+        })
+    }
+);
