c0b0d2b5805b90ae1f7a4c89816d753470db42e5
Nicolas Gauthier <sina@MacBook-Pro-de-Nicolas-2.local>
2020-11-07 19:32:07 +0100

    ajout de la visu des données v1

--- scriptprojet.js
+++ scriptprojet.js
@@ -1,1 +1,94 @@
 console.log("c'est ici qu'il faut coder votre javascript")
+
+var result;
+fetch('https://trouver.datasud.fr/dataset/973765d0-a2a8-442d-bf3b-aec4e70fdd69/resource/faa81490-2a38-42e5-8a43-3b326ba5fce9/download/geoserver-getfeature.application', { method: 'GET',
+    headers: {},
+    mode: 'cors',
+    cache: 'default'}).then(
+    function(response){
+        response.json().then(function(data){
+            console.log(data);
+            result = data;
+            console.log(result["features"][0]["properties"]["lib_dep"])
+        })
+    }
+);
+
+
+
+
+
+var oReq = new XMLHttpRequest();
+var url = "https://trouver.datasud.fr/dataset/ddc457a2-6516-46ae-bcd0-49ac317c5c4a/resource/bf2bc02a-9db4-4485-8eb7-97fd3a859eea/download/oreca-vehicules-par-carburant_yzshdad.xls";
+oReq.open("GET", url, true);
+oReq.responseType = "arraybuffer";
+
+var worksheetf
+
+oReq.onload = function(e) {
+    var arraybuffer = oReq.response;
+
+    /* convert data to binary string */
+    var data = new Uint8Array(arraybuffer);
+    var arr = new Array();
+    for(var i = 0; i != data.length; ++i) arr[i] = String.fromCharCode(data[i]);
+    var bstr = arr.join("");
+
+    /* Call XLSX */
+
+
+
+    var workbook = XLSX.read(bstr, {type:"binary"});
+
+    /* DO SOMETHING WITH workbook HERE */
+    var first_sheet_name = workbook.SheetNames[3];
+    /* Get worksheet */
+    var worksheet = workbook.Sheets[first_sheet_name];
+    worksheetf = XLSX.utils.sheet_to_json(worksheet,{raw:true});
+
+    var fd = JSON.stringify(worksheetf)
+    console.log(worksheetf)
+    console.log(worksheetf[104]['Source : SDES-RSVERO '].toUpperCase()) // genere le nom du departement
+    console.log(worksheetf[105]['__EMPTY_15'].toString()  + " beb")
+    console.log(typeof worksheetf[105]['Source : SDES-RSVERO ']);
+    console.log(typeof worksheetf[105]['__EMPTY_15'].toString());
+}
+
+oReq.send();
+
+
+function myFunction() {
+   // document.getElementById("demo").innerHTML = worksheetf[105]['Source : SDES-RSVERO '];
+    //document.getElementById("demo").innerHTML = worksheetf[106]['Source : SDES-RSVERO '];
+    var i;
+    for (i = 104; i!=110; i++) {
+        var paragraph = document.getElementById("demo");
+        var e = document.getElementById("dep");
+        var strUser = e.options[e.selectedIndex].text;
+       // console.log(e.options[e.selectedIndex].text)
+       // console.log(worksheetf[i]['Source : SDES-RSVERO '].toUpperCase())
+
+
+        if(e.options[e.selectedIndex].text.toUpperCase() === worksheetf[i]['Source : SDES-RSVERO '].toUpperCase() ){
+            document.getElementById("demo").innerHTML = "";
+
+
+            var str1 = document.createTextNode(worksheetf[i]['Source : SDES-RSVERO '] + "  " +
+                worksheetf[i]['__EMPTY'].toString() + "  " + worksheetf[i]['__EMPTY_1'].toString());
+            var saut = document.createElement("br");
+            paragraph.appendChild(str1);
+            paragraph.appendChild(saut);
+
+
+        }
+
+    }
+   // var x = document.getElementById("mySelect");
+   // var option = document.createElement("option");
+   // option.text = "Kiwi";
+   // x.add(option);
+}
+
+
+// Close the dropdown menu if the user clicks outside of it
+
